d4bdb75922f04a7bd8378d0f6b0dce9caa78fc37
Jacob <jacob.m.mcfaul@gmail.com>
2020-03-31 15:49:28 -0700

    added selector box for the puzzle as well as number inputs

--- sudoku/src/NoteBoxes.jsx
+++ sudoku/src/NoteBoxes.jsx
@@ -4,6 +4,7 @@ const NoteBoxes = () => {
   let notes = [];
   for (let i = 1; i < 10; i++) {
     notes.push(
+      // Keys are 1-9
       <div key={i} className="noteNumbers">
         {i}
       </div>
